7c3d40d191a708225dd7bb25157cd5f1a5b35d6a
robustTechie <captain.shank75@gmail.com>
2020-09-30 18:17:46 +0530

    new dialog

--- app/src/main/java/com/example/errorbox/ui/GameWonFragment.kt
+++ app/src/main/java/com/example/errorbox/ui/GameWonFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import com.example.errorbox.R
 import com.example.errorbox.databinding.FragmentGameWonBinding
 import com.example.errorbox.hints.HintAFragment
+import java.util.*
 
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +19,9 @@ class GameWonFragment : Fragment() {
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener{view: View->
             val hintAFragment = HintAFragment(context, activity)
-            hintAFragment.init("Password for next module zip file is 'amFOSS@pRoB3'")
+            val rand = Random()
+            val id: Int = rand.nextInt(1000)*1000
+            hintAFragment.init("You did it! The final level is in /home/Kernel. Good Luck! And remember to take a screenshot of this window. ID is: $id")
         }
         return binding.root
     }
